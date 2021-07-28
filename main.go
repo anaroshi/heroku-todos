5c81e3cf9803b9ad8361474571e0599a83735eaa
anaroshi <anaroshi@naver.com>
2021-07-29 01:53:50 +0900

    use port env var

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"sundorWeb/todos/app"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	m := app.MakeHandler("./test.db")
 	defer m.Close()
 	
 	log.Println("Started App")
-	err := http.ListenAndServe(":3000", m)	
+	err := http.ListenAndServe(":"+port, m)	
 	if err != nil {
 		panic(err)
 	}
